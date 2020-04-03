c98eb988116311be0cd2f2bb9282c937cf840e12
Tristan Ranee <tristanranee@gmail.com>
2020-04-04 00:59:45 +1100

    added website folders to git

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const altiumBar = document.querySelector('bar-altium')
+const spiceBar = document.querySelector('bar-spice')
+const matlabBar = document.querySelector('bar-matlab')
+const cBar = document.querySelector('bar-C')
+const pyhtonBar = document.querySelector('bar-python')
+
+var t1 = new TimelineLite()

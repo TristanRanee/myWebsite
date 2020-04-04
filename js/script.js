905b7faaa4cfc0a85f7f38bd96741b8ede1d61e4
Tristan Ranee <tristanranee@gmail.com>
2020-04-04 17:44:20 +1100

    Configured mobile navigation bar

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
-const altiumBar = document.querySelector('bar-altium')
-const spiceBar = document.querySelector('bar-spice')
-const matlabBar = document.querySelector('bar-matlab')
-const cBar = document.querySelector('bar-C')
-const pythonBar = document.querySelector('bar-python')
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".navLinks")
+    const navLinks = document.querySelectorAll(".navLinks li");
+    burger.addEventListener("click", () => {
+         //Toggle navigation
+        nav.classList.toggle("navActive");
+        //Animate Links
+        navLinks.forEach((link,index)=>{
+            if(link.style.animation){
+                link.style.animation = '';
+            }else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle')
+    });
+}
 
-var t1 = new TimelineLite()
-t1.fromTo(altiumBar, .75, {width: `calc(0%-6px)`}, {width: `calc(90%-6px)`, ease: Power4.easeOut})
-    .fromTo(spiceBar, .75, {width: `calc(0%-6px)`}, {width: `calc(90%-6px)`, ease: Power4.easeOut})
-    .fromTo(matlabBar, .75, {width: `calc(0%-6px)`}, {width: `calc(90%-6px)`, ease: Power4.easeOut})
-    .fromTo(cBar, .75 ,{width: `calc(0%-6px)`}, {width: `calc(90%-6px)`, ease: Power4.easeOut})
-    .fromTo(pythonBar, .75, {width: `calc(0%-6px)`}, {width: `calc(90%-6px)`, ease: Power4.easeOut})
+navSlide();
+
+/*
+const altiumBar = document.querySelector('bar-altium');
+const spiceBar = document.querySelector('bar-spice');
+const matlabBar = document.querySelector('bar-matlab');
+const cBar = document.querySelector('bar-C');
+const pythonBar = document.querySelector('bar-python');
+
+var t1 = new TimelineLite();
+t1.fromTo(altiumBar, .75, {width: `calc(0%-6px)`}, {width: `calc(90%-6px)`, ease: Power4.easeOut});
+t1.fromTo(spiceBar, .75, {width: `calc(0%-6px)`}, {width: `calc(90%-6px)`, ease: Power4.easeOut});
+t1.fromTo(matlabBar, .75, {width: `calc(0%-6px)`}, {width: `calc(90%-6px)`, ease: Power4.easeOut});
+t1.fromTo(cBar, .75 ,{width: `calc(0%-6px)`}, {width: `calc(90%-6px)`, ease: Power4.easeOut});
+t1.fromTo(pythonBar, .75, {width: `calc(0%-6px)`}, {width: `calc(90%-6px)`, ease: Power4.easeOut});
 
 
-const controller = new ScrollMagic.Controller()
-const scene = new ScrollMagic.Scene({triggerElement: '.skills', triggerHook: 0})
-.setTween(t1)
-.addTo(controller)
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({triggerElement: '.skills', triggerHook: 0});
+    .setTween(t1);
+    .addTo(controller);
+
+    */
